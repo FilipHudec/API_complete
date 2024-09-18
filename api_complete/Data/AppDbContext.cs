b1c1ef4f13fc917b9f7868d3972eb0b4960185a7
Filip Hudec <filip.hudec.022@pslib.cz>
2024-09-18 12:05:30 +0200

    second commit

--- api_complete/Data/AppDbContext.cs
+++ api_complete/Data/AppDbContext.cs
@@ -13,6 +13,42 @@ namespace api_complete.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
            base.OnModelCreating(modelBuilder);
+            // fluent
+            modelBuilder.Entity<Article>().HasData(
+                new Article
+                {
+                    ArticleId = 1,
+                    Title = "First Article",
+                    Content = "This is the content of the first article"
+                },
+                new Article
+                {
+                    ArticleId = 2,
+                    Title = "Second Article",
+                    Content = "This is the content of the second article"
+                }
+            );
+            modelBuilder.Entity<Comment>().HasData(
+                new Comment
+                {
+                    CommentId = 1,
+                    Text = "This is the first comment",
+                    ArticleId = 1
+                },
+                new Comment
+                {
+                    CommentId = 2,
+                    Text = "This is the second comment",
+                    ArticleId = 1
+                },
+                new Comment
+                {
+                    CommentId = 3,
+                    Text = "This is the third comment",
+                    ArticleId = 2
+                }
+
+                );
         }
     }
 }
---
+++ api_complete/Migrations/20240918095416_migrace2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api_complete.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrace2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Comment",
+                columns: new[] { "CommentId", "ArticleId", "Text", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1, "This is the first comment", 0 },
+                    { 2, 1, "This is the second comment", 0 },
+                    { 3, 2, "This is the third comment", 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Comment",
+                keyColumn: "CommentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Comment",
+                keyColumn: "CommentId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Comment",
+                keyColumn: "CommentId",
+                keyValue: 3);
+        }
+    }
+}
